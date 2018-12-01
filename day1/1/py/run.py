5a0578e37e94bb9cece9a05fdf62ec8482b280d1
Matthew Salazar <m@tthewsalazar.com>
2018-12-01 14:20:06 -0600

    Day 1, Part 1 in Python

---
+++ day1/1/py/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Answer for Advent of Code, Day 1.1"""
+
+import re
+
+def run():
+  freq = 0
+  with open('input.txt', 'r') as file:
+    for num in file:
+      freq += int(num)
+    print(freq)
+      
+if __name__ == '__main__':
+  run()
