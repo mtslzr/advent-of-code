b6238c2b054a603674fadfcd7437e7cd77354628
Matthew Salazar <m@tthewsalazar.com>
2019-12-01 22:19:07 -0500

    Solved 2019.1.1-2 in TypeScript

---
+++ 2019/ts/index.js
@@ -0,0 +1,35 @@
+"use strict";
+exports.__esModule = true;
+var day1 = require("./day1/day1");
+switch (process.argv[2]) {
+    case "day1":
+        day1.part1();
+        day1.part2();
+        break;
+    case "day2":
+    case "day3":
+    case "day4":
+    case "day5":
+    case "day6":
+    case "day7":
+    case "day8":
+    case "day9":
+    case "day10":
+    case "day11":
+    case "day12":
+    case "day13":
+    case "day14":
+    case "day15":
+    case "day16":
+    case "day17":
+    case "day18":
+    case "day19":
+    case "day20":
+    case "day21":
+    case "day22":
+    case "day23":
+    case "day24":
+    case "day25":
+    default:
+        console.log('Day not found.');
+}
