a669ff935978fe772f6520e82855a9b466f5573e
Matthew Salazar <m@tthewsalazar.com>
2019-12-02 11:50:59 -0500

    Solved 2019.2.2 in Python

--- 2019/py/day2/day2.py
+++ 2019/py/day2/day2.py
@@ -1,12 +1,50 @@
 #!/usr/bin/env python3
-"""Answer for Advent of Code 2019, Day 2.1."""
+"""Answer for Advent of Code 2019, Day 2."""
+
+import sys
 
 
 def part1():
     """Rebuild intcode from list."""
-    numbers = read_input('./day2/input.txt')
+    numbers = run_intcode(read_input('./day2/input.txt'))
+    print(numbers[0])
 
+
+def part2():
+    """Rebuild intcode from list."""
+    x = y = 0
+    while x <= 99:
+        while y <= 99:
+            out = run_intcode(read_input('./day2/input.txt'), x, y)
+            if out[0] == 19690720:
+                print((100 * x + y))
+                sys.exit()
+            y += 1
+        x += 1
+        y = 0
+
+
+def read_input(filename):
+    """Parse input file and return list."""
+    with open(filename, 'r') as file:
+        for line in file:
+            nums = line.split(',')
     x = 0
+    for num in nums:
+        nums[x] = int(num)
+        x += 1
+
+    nums[1] = 12
+    nums[2] = 2
+    return nums
+
+
+def run_intcode(numbers, noun=12, verb=2):
+    """Process intcode and return result."""
+    x = 0
+    numbers[1] = noun
+    numbers[2] = verb
+
     for num in numbers:
         if numbers[x] == 99:
             break
@@ -23,24 +61,4 @@ def part1():
             print('Something went wrong; got opcode {}'.format(numbers[x]))
         x += 4
 
-    print(numbers[0])
-
-
-def part2():
-    """Do part two."""
-    print("Part 2!")
-
-
-def read_input(filename):
-    """Parse input file and return list."""
-    with open(filename, 'r') as file:
-        for line in file:
-            nums = line.split(',')
-    x = 0
-    for num in nums:
-        nums[x] = int(num)
-        x += 1
-
-    nums[1] = 12
-    nums[2] = 2
-    return nums
+    return numbers
